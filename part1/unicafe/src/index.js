66c8fe2a2bb4908748131be18ef1a5946078dd34
konay <mr.mr.naymyoaung@gmail.com>
2020-09-22 19:49:45 +0630

    unicafe 1.11 finished

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,26 +1,27 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistic = props => <div>
-  <span>{props.text} {props.value}</span>
-</div>
+const Statistic = props => <tr><td>{props.text}</td><td>{props.value}</td></tr>
 
 const Statistics = (props) => {
   if (props.all === 0) return <div>No feedback given</div>
   return (
     <div>
-      <Statistic text="good" value={props.good} />
-      <Statistic text="neutarl" value={props.neutral} />
-      <Statistic text="bad" value={props.bad} />
-      <Statistic text="all" value={props.all} />
-      <Statistic text="average" value={props.average} />
-      <Statistic text="positive" value={props.positive} />
+      <table>
+        <tbody>
+          <Statistic text="good" value={props.good} />
+          <Statistic text="neutarl" value={props.neutral} />
+          <Statistic text="bad" value={props.bad} />
+          <Statistic text="all" value={props.all} />
+          <Statistic text="average" value={props.average} />
+          <Statistic text="positive" value={props.positive} />
+        </tbody>
+      </table>
     </div>
   )
 }
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
 
 const App = () => {
   // save clicks of each button to own state
@@ -28,6 +29,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const all = good + neutral + bad
+
   const positive = all > 0 ? (100 * good) / all + " %" : 0 + " %"
   const average = all > 0 ? (good - bad) / all : 0;
 
