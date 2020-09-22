b69ba2b1db4cb975837f44b304530f86f353c881
konay <mr.mr.naymyoaung@gmail.com>
2020-09-22 18:50:57 +0630

    unicafe 1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,32 +1,32 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Statistic = props => <div>
+  <span>{props.text} {props.value}</span>
+</div>
+
 const Statistics = (props) => {
-  if (props.all === 0 ) return <div>No feedback given</div>
+  if (props.all === 0) return <div>No feedback given</div>
   return (
     <div>
-      <span>good {props.good} </span><br />
-      <span>neutral {props.neutral} </span><br />
-      <span>bad {props.bad}</span> <br />
-      <span>all {props.all}</span> <br />
-      <span>average {props.average} </span> <br />
-      <span>positive {props.positive}</span>
+      <Statistic text="good" value={props.good} />
+      <Statistic text="neutarl" value={props.neutral} />
+      <Statistic text="bad" value={props.bad} />
+      <Statistic text="all" value={props.all} />
+      <Statistic text="average" value={props.average} />
+      <Statistic text="positive" value={props.positive} />
     </div>
   )
 }
-
-
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
   const all = good + neutral + bad
   const positive = all > 0 ? (100 * good) / all + " %" : 0 + " %"
   const average = all > 0 ? (good - bad) / all : 0;
-
 
   return (
     <div>
