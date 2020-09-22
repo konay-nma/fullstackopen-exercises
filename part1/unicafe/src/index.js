ca7ba360d67a4e63a6c0cdf1eede3f3dd40bb408
konay <mr.mr.naymyoaung@gmail.com>
2020-09-22 19:00:58 +0630

    unicafe 1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -19,6 +19,9 @@ const Statistics = (props) => {
   )
 }
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -31,9 +34,9 @@ const App = () => {
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>natural</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
     </div>
