e1ccaa1f08dafc2a48d10f40b589a70f2e43e605
Konay <konay@ubuntu.lan>
2020-09-29 14:05:38 +0630

    countries 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
-
   const [countries, setCountires] = useState([])
   const [filterCountry, setFilterCountry] = useState('')
 
