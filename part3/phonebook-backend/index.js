859153041078388879a44d27b9e69bdd39171b98
Konay <mr.mr.naymyoaung@gmail.com>
2020-10-04 20:33:50 +0630

    phonebook-backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -29,6 +29,14 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`running server on port ${PORT}`)
